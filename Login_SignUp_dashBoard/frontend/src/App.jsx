ee0f72634043835cff37c7500ed5db34c441c3d5
Nikhil P <nikhilpandravesham160802@gmail.com>
2024-08-26 11:57:11 +0530

    update from testing branch

--- Login_SignUp_dashBoard/frontend/src/App.jsx
+++ Login_SignUp_dashBoard/frontend/src/App.jsx
@@ -1,24 +1,3 @@
-// // import { useEffect, useState } from 'react'
-// import { useEffect} from 'react'
-// export default function App() {
-// // const [data, setData] = useState([]);
-
-// useEffect(()=> {
-// fetch('http://localhost:8081/signupdetails')
-// .then(res => res.json())
-// .then(data =>console.log(data))
-// .catch(err => console.log("this is error",err));
-// },[])
-
-// return (
-// <div>
-  
-// </div>
-// )
-
-// }
-
-
 import LoginForm from "./components/Login_form"
 import SignUpForm from "./components/SignUpForm"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
